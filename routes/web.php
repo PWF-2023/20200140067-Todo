52f2333c7ed590dd2ade9a24534e50b35fb6d4c4
Tiarasekarsini <tiara.sekarsini.ft20@mail.umy.ac.id>
2023-03-26 14:23:11 +0700

    env, mvc, route, relationship

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,6 +28,13 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+    Route::get('/todo', [App\Http\Controllers\TodoController::class, 'index'])->name('todo.index');
+    Route::get('/todo/create', [App\Http\Controllers\TodoController::class,'create'])->name('todo.create');
+    Route::get('/todo/edit', [App\Http\Controllers\TodoController::class,'edit'])->name('todo.edit');
+
+    Route::get('/user', [App\Http\Controllers\UserController::class,'index'])->name('user.index');
 });
 
 require __DIR__.'/auth.php';
